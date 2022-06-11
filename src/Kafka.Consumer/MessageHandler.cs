dace577ba429ca2bff90254c432c8b52287f4ae6
Marcos Levy <marcossistema@gmail.com>
2022-06-11 15:41:26 -0300

    Kafka com ASP.NET Core

---
+++ src/Kafka.Consumer/MessageHandler.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kafka.Consumer
+{
+    public class MessageHandler : IHostedService
+    {
+        private readonly ILogger _logger;
+        public MessageHandler(ILogger<MessageHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "filateste-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var item = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                item.Subscribe("filaTeste");
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        var message = item.Consume(cts.Token);
+                        _logger.LogInformation($"Mensagem: {message.Value} recebida de {message.TopicPartition}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    item.Close();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Kafka.Producer/Controllers/ProducerController.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Kafka.Producer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class ProducerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created("", SendMessage(message));
+        }
+
+        private string SendMessage(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var sendResult = producer
+                        .ProduceAsync("filaTeste", new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+
+                    return $"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'";
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Erro ao enviar mensagem: {e.Error.Reason}");
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
